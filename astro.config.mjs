13deaab00e5d6394c5b82bd1307be0f9ec94cde5
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-06-16 18:26:28 +0800

    build: file fetching, markdowns, and subdirectories routes deployment test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://choco-melk.github.io',
-    base: '/portfolio'
+    base: '/portfolio',
+    outDir: './portfolio',
 });
