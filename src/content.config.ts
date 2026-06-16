49e11ef8d1ac0b6cb41813ed626d0c51c571fcf5
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-06-16 23:27:30 +0800

    ui: added frontend for project page

---
+++ src/content.config.ts
@@ -0,0 +1,28 @@
+import { file, glob } from "astro/loaders"
+import { defineCollection } from 'astro:content'
+import { z } from "astro/zod"
+
+const projects = defineCollection({
+    loader: file("src/projects.json"),
+    schema: z.object({
+        id: z.number(),
+        name: z.string(), 
+        image: z.string().optional(),
+        "tech-stack": z.array(z.string()),
+        description: z.string(),
+        "project-link": z.string().optional(),
+        "github-link": z.string().optional()
+    })
+})
+
+const blogs = defineCollection({
+    loader: glob({ base: "src/blogs/", pattern: "*.md"}),
+    schema: z.object({
+        image: z.string().optional(),
+        date: z.string(),
+        title: z.string(),
+        preview: z.string()
+    })
+})
+
+export const collections = { blogs, projects }
